af415efbda182a00649605f92f254989f60442a1
flozzone <e0925917@student.tuwien.ac.at>
2015-12-01 17:34:09 +0100

    cleanup python c wrapper.
    Signed-off-by: flozzone <e0925917@student.tuwien.ac.at>

--- hashing/sherlock/sherlockmodule.c
+++ hashing/sherlock/sherlockmodule.c
@@ -7,39 +7,21 @@ sherlock_signature(PyObject *self, PyObject *args)
     const char *filepath;
     Sig *ret_sig;
 
-    printf("Start py function\n");
-
     if (!PyArg_ParseTuple(args, "s", &filepath))
         return NULL;
 
     ret_sig = signaturep(filepath);
 
-    //PyObject* list = PyList_New(ret_sig->nval);
     PyObject* list = PyList_New(0);
-
-    printf("got a list of %i signatures\n", ret_sig->nval);
 
     int i = 0;
     while (i < ret_sig->nval) {
-
-        //printf("process sig i:%i\n", i);
-
         PyObject *val = Py_BuildValue("l", ret_sig->val[i]);
-
-        //printf("adding %lu ... ", ret_sig->val[i]);
-
         PyList_Append(list, val);
-
-        //printf("added\n");
         i++;
     }
 
-
     assert(PyList_Check(list) == 1);
-
-    printf("size: %i\n", (int) PyList_Size(list));
-
-    printf("returning list\n");
 
     return list;
 }
